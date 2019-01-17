bccaa2aa43173167087dd1d493f14c673c0a7161
Azeem Mirza <azeem.mirza94@gmail.com>
2019-01-17 21:24:03 +0500

    Async request dev
    1. async request
    2. form naming

--- public/script.js
+++ public/script.js
@@ -6,6 +6,15 @@ window.onload = function () {
         e.preventDefault();
         var form = document.getElementById('form');
         var formData = new FormData(form);
+        var xhr = new XMLHttpRequest();
 
-    })
+        xhr.open('POST', '/server', true);
+        xhr.onload = function (e) {
+            console.log(e);
+        };
+        xhr.onerror = function (e) {
+            console.log(e);
+        };
+        xhr.send(formData);
+    });
 };
